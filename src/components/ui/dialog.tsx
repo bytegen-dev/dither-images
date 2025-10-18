a004bcd09a8a1ee5eb2c6471e961c537c113c0d5
Isaac. <bytegenlabs@gmail.com>
2025-10-18 07:41:42 +0100

    fix svg export

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -5,7 +5,6 @@ import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
 
 import { cn } from "@/lib/utils";
-import { textAlign } from "html2canvas/dist/types/css/property-descriptors/text-align";
 
 const Dialog = DialogPrimitive.Root;
 
